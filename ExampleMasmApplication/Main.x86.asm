1ff86f65bcffeeb2b3c2592511b3cc0f8c4a05c9
visuve <veli-matti.visuri@iki.fi>
2023-03-18 09:45:00 +0200

    Improve Main.x86.asm

--- ExampleMasmApplication/Main.x86.asm
+++ ExampleMasmApplication/Main.x86.asm
@@ -1,11 +1,10 @@
 IFDEF X86
-.386
-.MODEL TINY, SYSCALL
-.STACK 1024
+.486
+.MODEL FLAT, C
 
-EXTERN _GetStdHandle@4: PROC
-EXTERN _WriteConsoleA@20: PROC
-EXTERN _ExitProcess@4: PROC
+EXTERN GetStdHandle@4: PROC
+EXTERN WriteConsoleA@20: PROC
+EXTERN ExitProcess@4: PROC
 
 .CONST
 StdOutputHandle = -11 ; STD_OUTPUT_HANDLE
@@ -20,7 +19,7 @@ HelloWorldMain PROC
 
 	; HANDLE stdout = GetStdHandle(STD_OUTPUT_HANDLE);
 	push StdOutputHandle
-	call _GetStdHandle@4
+	call GetStdHandle@4
 
 	; WriteConsoleA(stdout, HelloMessage, HelloMessageLen, bytesWritten, 0);
 	push 0
@@ -28,11 +27,11 @@ HelloWorldMain PROC
 	push HelloMessageLen - 1 ; Exclude null terminator
 	push offset HelloMessage
 	push eax
-	call _WriteConsoleA@20
+	call WriteConsoleA@20
 
 	; ExitProcess(0)
 	push 0
-	call _ExitProcess@4
+	call ExitProcess@4
 
 HelloWorldMain ENDP
 
