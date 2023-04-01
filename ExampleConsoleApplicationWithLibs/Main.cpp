e1e1bb9cbdc4dd7b13c55f4849e4cd67edfb7b43
visuve <veli-matti.visuri@iki.fi>
2023-04-01 15:53:16 +0300

    Remove the Timing class
    - std streams have supported chrono for a while

--- ExampleConsoleApplicationWithLibs/Main.cpp
+++ ExampleConsoleApplicationWithLibs/Main.cpp
@@ -1,22 +1,23 @@
 #include "PCH.hpp"
-#include "Timing.hpp"
 
 #include "../ExampleSharedLibrary/ExampleSharedLibrary.h"
 #include "../ExampleStaticLibrary/ExampleStaticLibrary.hpp"
 
 int wmain(/*int argc, wchar_t* argv[], wchar_t* envp[]*/)
 {
-	using namespace Timing;
-
 	{
-		Timer timer;
-
+		auto before = std::chrono::high_resolution_clock::now();
 		ExampleSharedLibraryHello();
+		auto after = std::chrono::high_resolution_clock::now();
+
+		std::cout << "ExampleSharedLibraryHello took " << after - before << std::endl;
 	}
 	{
-		Timer timer;
-
+		auto before = std::chrono::high_resolution_clock::now();
 		ExampleStaticLibrary::Hello();
+		auto after = std::chrono::high_resolution_clock::now();
+
+		std::cout << "ExampleStaticLibrary::Hello took " << after - before << std::endl;
 	}
 
 	return 0;
--- ExampleConsoleApplicationWithLibs/PCH.hpp
+++ ExampleConsoleApplicationWithLibs/PCH.hpp
@@ -1,7 +1,4 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
-#include <SDKDDKVer.h>
-
 #include <chrono>
 #include <iostream>
